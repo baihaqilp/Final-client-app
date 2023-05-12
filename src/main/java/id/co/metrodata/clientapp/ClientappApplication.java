42f69a9376bdd92c029415918e170b49d5e4c795
baihaqilp <baihaqiluhurpangestika@gmail.com>
2023-05-12 22:11:03 +0700

    first initialize

---
+++ src/main/java/id/co/metrodata/clientapp/ClientappApplication.java
@@ -0,0 +1,13 @@
+package id.co.metrodata.clientapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/co/metrodata/clientapp/ClientappApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.metrodata.clientapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
