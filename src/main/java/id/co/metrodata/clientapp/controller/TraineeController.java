0d995e9dccb4b37c2056b6b0b4511bebe07f98f7
Zadid Taqwa <Zadidtaqwa123@gmail.com>
2023-05-18 23:46:32 +0700

    View Class Trainee

---
+++ src/main/java/id/co/metrodata/clientapp/controller/TraineeController.java
@@ -0,0 +1,24 @@
+package id.co.metrodata.clientapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Controller
+@RequestMapping("/trainee")
+@AllArgsConstructor
+public class TraineeController {
+
+  @GetMapping
+  private String dashboard() {
+    return "trainee/index";
+  }
+
+  @GetMapping("/class")
+  private String traineeClass() {
+    return "trainee/class/class";
+  }
+}
