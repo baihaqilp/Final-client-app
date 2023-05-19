7f198648169cf31b38d5f27f753b547fc3c7887c
baihaqilp <baihaqiluhurpangestika@gmail.com>
2023-05-20 01:48:47 +0700

    trainer fix

--- src/main/java/id/co/metrodata/clientapp/controller/TrainerController.java
+++ src/main/java/id/co/metrodata/clientapp/controller/TrainerController.java
@@ -1,14 +1,24 @@
 package id.co.metrodata.clientapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import id.co.metrodata.clientapp.service.ClassroomService;
+import id.co.metrodata.clientapp.service.SegmenService;
+import lombok.AllArgsConstructor;
+
 @Controller
-@RequestMapping("/trainer")
+@RequestMapping("/Trainer")
+@AllArgsConstructor
 public class TrainerController {
 
-    @GetMapping
+    private SegmenService segmenService;
+    private ClassroomService classroomService;
+
+    @GetMapping("/dashboard")
     public String dashboard() {
         return "trainer/index";
     }
@@ -28,8 +38,49 @@ public class TrainerController {
         return "trainer/submission/submission";
     }
 
-    @GetMapping("/task")
+    @GetMapping("/classroom/segment/task")
     public String taskTrainer() {
         return "trainer/task/task";
     }
+
+    @GetMapping("/materi")
+    public String materi() {
+        return "trainer/materi/materi";
+    }
+
+    @GetMapping("/materi-add")
+    public String materiAdd() {
+        return "trainer/materi/addMateri";
+    }
+
+    @GetMapping("/materi-edit")
+    public String materiEdit() {
+        return "trainer/materi/editMateri";
+    }
+
+    @GetMapping("/grade")
+    public String gradeAdmin() {
+        return "trainer/grade/grade";
+    }
+
+    @GetMapping("/trainee")
+    public String traineeTrainer() {
+        return "trainer/trainee/trainee";
+    }
+
+    @GetMapping("/trainee-detail")
+    public String traineeDetailTrainer() {
+        return "trainer/trainee/detailTrainee";
+    }
+
+    @GetMapping("/classroom/{id}")
+    public String trainerSegment(@PathVariable long id, Model model) {
+        model.addAttribute("classroom", classroomService.getById(id));
+        return "trainer/segment/segment";
+    }
+
 }
+
+    
+
+    
---
+++ src/main/resources/static/js/trainer/segment.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+  const cardContent = $(".card-content");
+  let class_id = $("#class_id");
+  $.ajax({
+    url: "/api/segment/class/" + class_id, // Replace with your API endpoint
+    method: "GET",
+    success: function (response) {
+      response.forEach(function (segment) {
+        const segmentCard = $("<div>").addClass("card mb-3");
+        const cardBody = $("<div>").addClass("card-body");
+
+        const segmentTitle = $("<h5>")
+          .addClass("card-title pb-2")
+          .text(segment.classroom.program.name);
+        const classTitle = $("<h5>")
+          .addClass("card-title")
+          .text(segment.classroom.name);
+
+        const dateContainer = $("<div>").addClass(
+          "container border-bottom mb-3"
+        );
+        const dateCardBody = $("<div>").addClass(
+          "card-body d-flex justify-content-between align-items-center"
+        );
+        const startDateTitle = $("<h5>")
+          .addClass("card-title")
+          .text("Start Date");
+        const startDateText = $("<p>")
+          .addClass("card-text")
+          .text(segment.start_date);
+        const endDateTitle = $("<h5>").addClass("card-title").text("End Date");
+        const endDateText = $("<p>")
+          .addClass("card-text")
+          .text(segment.end_date);
+
+        // Append the elements to the HTML structure
+        dateCardBody.append(startDateTitle, startDateText);
+        dateCardBody.append(endDateTitle, endDateText);
+        dateContainer.append(dateCardBody);
+        cardBody.append(segmentTitle, classTitle, dateContainer);
+        segmentCard.append(cardBody);
+        cardContent.append(segmentCard);
+      });
+    },
+    error: function (xhr, status, error) {
+      console.error("Error:", error);
+    },
+  });
+});
