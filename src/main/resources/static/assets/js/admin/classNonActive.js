32751b42080f626265b6d987280c922f96b38572
baihaqilp <baihaqiluhurpangestika@gmail.com>
2023-05-27 17:37:12 +0700

    add class History

--- src/main/resources/static/assets/js/admin/classNonActive.js
+++ src/main/resources/static/assets/js/admin/classNonActive.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+
+
     $("#table-class").DataTable({
         ajax: {
             url: "/api/classroom/noactive",
@@ -31,6 +33,53 @@ $(document).ready(function () {
                     }
                 },
             },
+            {
+                data: null,
+                render: (data, type, row, meta) => {
+                    return `
+            <div class="dropdown">
+                <button class="btn btn-primary dropdown-toggle me-1" type="button"
+                    id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true"
+                    aria-expanded="false">
+                    Action
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                  <div class="dropwdown-item">
+                  <a href="/admin/class/${data.id}"
+                    type="button"
+                    class="p-3")"
+                  >
+                    Detail
+                  </a>
+                </div>
+                <div class="dropwdown-item mt-2">
+                  <a href="/admin/class/${data.id}/trainee"
+                    type="button"
+                    class="p-3"
+                  >
+                    Trainee List
+                  </a>
+                </div>
+                </div>
+            </div>
+            
+            `;
+                },
+            },
         ],
     });
 });
+
+function getById(id) {
+    $.ajax({
+        method: "GET",
+        url: "/api/classroom/" + id,
+        // beforeSend: addCsrfToken(),
+        dataType: "JSON",
+        beforeSend: addCsrfToken(),
+        success: (res) => {
+            $("#detail_name").val(res.name);
+        },
+    });
+}
+
